25dc39c6e1c3b5db752ebc4219d292ffe75f320d
Dmitry Lisachenko <xdemonx07@gmail.com>
2019-04-28 17:01:17 +0300

    dev

--- qtor-core/include/qtor/view_manager.hpp
+++ qtor-core/include/qtor/view_manager.hpp
@@ -22,4 +22,89 @@ namespace qtor
 		/// release view, decrements view counter. If there are no more connected views - pauses subscription
 		unsigned view_release();
 	};
+
+
+	/// similar to std::shared_ptr, but has unique_ptr semantics,
+	/// and calls view_addref, view_release on acquire/release operations
+	template <class Store>
+	class view_manager_ref
+	{
+	public:
+		static_assert(std::is_base_of_v<view_manager, Store>);
+
+		using store_type = Store;
+		using pointer_type = std::shared_ptr<store_type>;
+		using element_type = typename pointer_type::element_type;
+
+	private:
+		pointer_type m_ptr;
+
+	public:
+		void reset() { reset(nullptr); }
+		void reset(pointer_type ref);
+		pointer_type release() { return std::move(m_ptr); }
+
+		auto get() const noexcept { return m_ptr.get(); }
+		const auto & get_smart_ptr() const noexcept { return  m_ptr; }
+
+		explicit operator bool() const noexcept { return static_cast<bool>(m_ptr); }
+		operator pointer_type() const noexcept { return m_ptr; }
+
+		auto * operator ->() const noexcept { return m_ptr.get(); }
+		decltype(auto) operator *() const noexcept { return *m_ptr; }
+
+	public:
+		void swap(view_manager_ref & other) { m_ptr.swap(other.m_ptr); }
+
+	public:
+		view_manager_ref() = default;
+		~view_manager_ref();
+
+		view_manager_ref(pointer_type ptr);
+		view_manager_ref & operator =(pointer_type ptr);
+
+		view_manager_ref(const view_manager_ref &) = delete;
+		view_manager_ref & operator =(const view_manager_ref &) = delete;
+	};
+
+	template <class Store>
+	inline view_manager_ref<Store>::view_manager_ref(pointer_type ptr)
+	    : m_ptr(std::move(ptr))
+	{
+		if (m_ptr)
+			m_ptr->view_addref();
+	}
+
+	template <class Store>
+	inline auto view_manager_ref<Store>::operator=(pointer_type ptr) -> view_manager_ref &
+	{
+		reset(std::move(ptr));
+		return *this;
+	}
+
+	template <class Store>
+	inline view_manager_ref<Store>::~view_manager_ref()
+	{
+		if (m_ptr)
+			m_ptr->view_release();
+	}
+
+	template <class Store>
+	void view_manager_ref<Store>::reset(pointer_type ref)
+	{
+		if (m_ptr == ref) return;
+
+		if (m_ptr)
+			m_ptr->view_release();
+
+		m_ptr = std::move(ref);
+		if (m_ptr)
+			m_ptr->view_addref();
+	}
+
+	template <class Store>
+	inline void swap(view_manager_ref<Store> & v1, view_manager_ref<Store> & v2)
+	{
+		v1.swap(v2);
+	}
 }
--- qtor-core/src/TorrentCategoryModel.cpp
+++ qtor-core/src/TorrentCategoryModel.cpp
@@ -1,24 +1,170 @@
 #include <qtor/TorrentCategoryModel.hqt>
+#include <QtTools/Utility.hpp>
+#include <QtCore/QLocale>
 
 namespace qtor
 {
-	void TorrentCategoryModel::InitStatusItems()
+	QString TorrentCategoryModel::NameForCategory(category_type cat) const
 	{
-		m_categories.push_back({all, 0});
-		m_categories.push_back({downloading, 0});
-		m_categories.push_back({downloaded, 0});
-		m_categories.push_back({active, 0});
-		m_categories.push_back({nonactive, 0});
-		m_categories.push_back({stopped, 0});
-		m_categories.push_back({error, 0});
-
+		switch (cat)
+		{
+			case category_type::all:             return tr("all");
+			case category_type::downloading:     return tr("downloading");
+			case category_type::downloaded:      return tr("downloaded");
+			case category_type::active:          return tr("active");
+			case category_type::nonactive:       return tr("nonactive");
+			case category_type::stopped:         return tr("stopped");
+			case category_type::error:           return tr("error");
+			default:                             return tr("Unknown category");
+		}
 	}
 
-	void TorrentCategoryModel::UpdateData(const torrent * first, const torrent * last)
+	QIcon TorrentCategoryModel::IconForCategory(category_type cat) const
 	{
-		for (; first != last; ++first)
+		switch (cat)
 		{
+			case category_type::all:             //return QIcon::fromTheme("");
+			case category_type::downloading:     //return QIcon::fromTheme("");
+			case category_type::downloaded:      //return QIcon::fromTheme("");
+			case category_type::active:          //return QIcon::fromTheme("");
+			case category_type::nonactive:       //return QIcon::fromTheme("");
+			case category_type::stopped:         //return QIcon::fromTheme("");
+			case category_type::error:           //return QIcon::fromTheme("");
+			default:                             return {};
+		}
+	}
 
+	auto TorrentCategoryModel::category(const torrent & torr) noexcept -> category_set
+	{
+		category_set result;
+		const auto status = torr.status();
+		const auto requested_size = torr.requested_size();
+		const auto current_size = torr.current_size();
+		const auto error_string = torr.error_string();
+
+		const bool all = true;
+		const bool error = error_string.has_value();
+		const bool stopped = status == torrent_status::stopped;
+		const bool active = not stopped and not error;
+		const bool nonactive = not stopped;
+		const bool downloading = status == torrent_status::downloading or status == torrent_status::downloading_queued;
+		const bool downloaded = current_size == requested_size;
+
+		result.set(category_type::all, all);
+		result.set(category_type::error, error);
+		result.set(category_type::stopped, stopped);
+		result.set(category_type::active, active);
+		result.set(category_type::nonactive, nonactive);
+		result.set(category_type::downloading, downloading);
+		result.set(category_type::downloaded, downloaded);
+
+		return result;
+	}
+
+	void TorrentCategoryModel::InitStatusItems()
+	{
+		m_categories.push_back({.category = all});
+		m_categories.push_back({.category = downloading});
+		m_categories.push_back({.category = downloaded});
+		m_categories.push_back({.category = active});
+		m_categories.push_back({.category = nonactive});
+		m_categories.push_back({.category = stopped});
+		m_categories.push_back({.category = error});
+
+		for (auto & cat : m_categories)
+		{
+			cat.count = 0;
+			cat.icon = IconForCategory(cat.category);
+			cat.name = NameForCategory(cat.category);
+		}
+	}
+
+	void TorrentCategoryModel::LanguageChanged()
+	{
+		beginResetModel();
+		InitStatusItems();
+		RecalculateData();
+		endResetModel();
+	}
+
+	void TorrentCategoryModel::LocaleChanged()
+	{
+		int first = 0;
+		int last  = qint(m_categories.size() - 1);
+		Q_EMIT dataChanged(index(first), index(last));
+	}
+
+	void TorrentCategoryModel::RecalculateData()
+	{
+		for (auto & category_item : m_categories)
+			category_item.count =  0;
+
+		if (m_torrent_store)
+		{
+			for (const torrent & tor : *m_torrent_store)
+			{
+				auto catset = category(tor);
+
+				if (catset[all])         m_categories[0].count += 1;
+				if (catset[downloading]) m_categories[1].count += 1;
+				if (catset[downloaded])  m_categories[2].count += 1;
+				if (catset[active])      m_categories[3].count += 1;
+				if (catset[nonactive])   m_categories[4].count += 1;
+				if (catset[stopped])     m_categories[5].count += 1;
+				if (catset[error])       m_categories[6].count += 1;
+			}
+		}
+
+		int first = 0;
+		int last  = qint(m_categories.size() - 1);
+		Q_EMIT dataChanged(index(first), index(last));
+	}
+
+	void TorrentCategoryModel::SetTorrentStore(std::shared_ptr<torrent_store> store)
+	{
+		if (store)
+		{
+			m_torrent_store = std::move(store);
+
+			auto callback = [this](auto && ...) { RecalculateData(); };
+			m_on_update_conn = m_torrent_store->on_update(callback);
+			m_on_erase_conn = m_torrent_store->on_erase(callback);
+			m_on_clear_conn = m_torrent_store->on_clear(callback);
+		}
+		else
+		{
+			m_on_update_conn.disconnect();
+			m_on_erase_conn.disconnect();
+			m_on_clear_conn.disconnect();
+
+			m_torrent_store = std::move(store);
+		}
+
+		RecalculateData();
+	}
+
+	int TorrentCategoryModel::rowCount(const QModelIndex & parent) const
+	{
+		return qint(m_categories.size());
+	}
+
+	auto TorrentCategoryModel::GetItem(int row) const -> CategoryItem
+	{
+		return m_categories.at(row);
+	}
+
+	QVariant TorrentCategoryModel::data(const QModelIndex & index, int role) const
+	{
+		int row = index.row();
+		if (row >= qint(m_categories.size())) return {};
+
+		auto loc = QLocale();
+		const auto & item = GetItem(row);
+		switch (role)
+		{
+			case Qt::DisplayRole:    return QStringLiteral("%1 (%2)").arg(item.name).arg(loc.toString(static_cast<qulonglong>(item.count)));
+			case Qt::DecorationRole: return item.icon;
+			default: return QVariant();
 		}
 	}
 }
